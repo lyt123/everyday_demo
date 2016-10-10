893da7137e2c902e30ed5eb266e5551125c9d8fc
lyt123 <1067081452@qq.com>
2016-10-10 09:14:27 +0800

    roote : using index.php to redirect every access

--- readme.txt
+++ readme.txt
@@ -2,3 +2,4 @@
 2.long_polling : using javascript to request php interface by setting a timeout
 3.redirect : redirect in php and javascript
 4.server : print all the key-values in array $_SERVER
+5.roote : an easy way to roote, namely, redirect the request url to another
--- roote/index.php
+++ roote/index.php
@@ -1,18 +1,22 @@
 <?php
+/*
+ * enter "http://localhost/everyday_demo/roote/server" into url
+ * will go in index.php(by .htaccess setting)
+ * thus redirect to "http://localhost/everyday_demo/server/server_values"
+*/
 $path = ltrim($_SERVER['REQUEST_URI'], '/');// Trim leading slash(es)
 $elements = explode('/', $path);// Split path on slashes
-switch (array_pop($elements))// Pop off first item and switch
+switch (array_pop($elements))// Pop off final item and switch, in this example, it's "server"
 {
-    case'roote.php':
+    case'long_polling':
         redirect('http://localhost/everyday_demo/long_polling/long_poller.html');
-        exit();
         break;
-    case'more':
-        redirect('localhost/everyday_demo/server_values.php');
+    case'server':
+        redirect('http://localhost/everyday_demo/server/server_values.php');
         break;
     default:
         header('HTTP/1.1 404 Not Found');
-//            redirect('http://github.com/lyt123/everyday_demo');
+        var_dump('nice');
 }
 
 function redirect($url, $statusCode = 301)

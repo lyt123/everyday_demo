6e35b74a67f878b09e2a6d4ce32b3c8848a33fa9
lyt123 <1067081452@qq.com>
2016-10-18 17:01:30 +0800

    extract img from html

--- forceutf8/my_test.php
+++ forceutf8/my_test.php
@@ -3,10 +3,12 @@ require_once ("Encoding.php");
 
 use ForceUTF8\Encoding;
 
+//fix the messy code to utf-8 encoding
 //$string = "FÃÂ©dération Camerounaise de Football\n";
-//$utf8_string = Encoding::fixUTF8($string);//fix the messy code to utf-8 encoding
-$string = file_get_contents("test/data/test1Latin.txt");
 //$utf8_string = Encoding::fixUTF8($string);
+
+//turn the file encoded with other code into utf-8
+$string = file_get_contents("test/data/test1Latin.txt");
 $utf8_string = Encoding::toUTF8($string);
 
 die($utf8_string);
